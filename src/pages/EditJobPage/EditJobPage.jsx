cfeab203b5b308ec17455b3a4a46cad8a3ca3c0b
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-11 10:32:24 -0500

    Add Edit Page

---
+++ src/pages/EditJobPage/EditJobPage.jsx
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class EditJobPage extends Component {
+  state = {
+    invalidForm: false,
+    formData: this.props.location.state.job
+  };
+
+  formRef = React.createRef();
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleUpdateJob(this.state.formData);
+  };
+
+  handleChange = e => {
+    const formData = {...this.state.formData, [e.target.name]: e.target.value};
+    this.setState({
+      formData,
+      invalidForm: !this.formRef.current.checkValidity()
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Edit Job</h1>
+        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Position:</label>
+            <input
+            //   className="form-control"
+              name="position"
+              value={this.state.formData.name}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-xs"
+            disabled={this.state.invalidForm}
+          >
+            SAVE JOB
+          </button>&nbsp;&nbsp;
+          <Link to='/'>CANCEL</Link>
+        </form>
+      </>
+    );
+  }
+}
+
+export default EditJobPage;
